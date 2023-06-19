ec5ee84a2476eb08a8de69f4dd7e3ca6ab014afa
estebanbosch <ADMIN@DESKTOP-EPS39UI>
2023-06-19 11:03:37 -0300

    PostMapping de VueloController para guardar un nuevo vuelo.

--- src/main/java/com/daos/aero/controller/VueloController.java
+++ src/main/java/com/daos/aero/controller/VueloController.java
@@ -1,8 +1,12 @@
 package com.daos.aero.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -10,16 +14,24 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.daos.aero.dto.VueloDTO;
 import com.daos.aero.excepcion.Excepcion;
 import com.daos.aero.model.Vuelo;
 import com.daos.aero.service.VueloService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/vuelos")
@@ -27,11 +39,13 @@ public class VueloController {
 
 	@Autowired
 	private VueloService service;
+	@Autowired
+	private VueloService vueloService;
 
 	/**
 	 * Permite filtrar Vuelos
-	 * Ej1 curl --location --request GET 'http://localhost:8081/vuelos?tipoVuelo=Comercial Lista los vuelos Comerciales
-	 * Ej2 curl --location --request GET 'http://localhost:8081/vuelos Lista todos los Vuelos
+	 * Ej1 curl --location --request GET 'http://localhost:8081/vuelos?tipoVuelo=Comercial' Lista los vuelos Comerciales
+	 * Ej2 curl --location --request GET 'http://localhost:8081/vuelos' Lista todos los Vuelos
 	 * @param tipoVuelo
 	 * @return
 	 * @throws Excepcion
@@ -87,5 +101,58 @@ public class VueloController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
+	/**
+	 * Insertar un nuevo Vuelo
+	 *       curl --location --request POST 'http://localhost:8081/vuelos' 
+	 *			--header 'Accept: application/json' 
+	 * 			--header 'Content-Type: application/json' 
+	 *			--data-raw '{
+	 *			    "nro": 3,
+	 *			    "fecha": "2023-06-25",
+	 *			    "destino": "Bariloche",
+	 *			    "nroFilas": 15,
+	 *				"nroAsientosPorFila": 8,
+	 *				"tipoVuelo": "Comercial"
+	 *			}'
+	 * 
+	 * @param v Vuelo a insertar
+	 * @return Vuelo insertado o error
+	 * @throws Exception
+	 */
+	@PostMapping
+	public ResponseEntity<Object> guardar(  @Valid @RequestBody VueloForm form, BindingResult result) throws Exception
+	{
+		if(result.hasErrors()) 
+		{
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(this.formatearError(result));
+		}
+		Vuelo v = form.toPojo();
+		v.setOrigen("Sauce Viejo");
+		v.setEstado("Registrado");
+		
+		service.inserta(v);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{nro}")
+				.buildAndExpand(v.getNro()).toUri();
+		return ResponseEntity.created(location).build();
 	
+	} 
+	
+	
+	private String formatearError(BindingResult result) throws JsonProcessingException
+	{
+		List<Map<String, String>> errores = result.getFieldErrors().stream().map(err ->  {
+															Map<String, String> error= new HashMap<>();
+															error.put(err.getField(),err.getDefaultMessage() );
+															return error;
+														}).collect(Collectors.toList());
+		MensajeError err=new MensajeError();
+		err.setCodigo("01");
+		err.setMensajes(errores);
+		
+		//ahora usamos la librería Jackson
+		ObjectMapper maper = new ObjectMapper();
+		String json = maper.writeValueAsString(err);
+		return json;
+		
+	}
 }
--- src/main/java/com/daos/aero/controller/VueloForm.java
+++ src/main/java/com/daos/aero/controller/VueloForm.java
@@ -1,102 +1,82 @@
-package com.daos.aero.model;
+package com.daos.aero.controller;
 
 import java.sql.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.daos.aero.model.Vuelo;
+
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 
-@Entity
-public class Vuelo {
-
-	@Id
-	@NotNull
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+public class VueloForm {
+	
+	@NotNull(message = "el número de vuelo no puede ser nulo")
 	private Long nro;
 
 	@DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
-	@Column(name = "fecha", nullable = false)
-	private Date fecha;
-	
-	@Column(name = "numero_filas")
-	private int nroFilas;
-	@Column(name = "numero_asientos_fila")
-	private int nroAsientosPorFila;
-	@Column(name = "tipo_vuelo")
-	private String tipoVuelo;
 	@NotNull
+	@FutureOrPresent(message="la fecha debe ser actual o futura")
+	private Date fecha;
+	@Max(value=30, message = "demasiadas filas")
+	private int nroFilas;
+	@Max(value=8, message = "demasiados asientos por fila")
+	private int nroAsientosPorFila;
+	private String tipoVuelo;
+	@Nonnull
 	private String destino;
-	private String origen;
-	private String estado;
-
-	// Getters and Setters
-	public long getNro() {
+	
+	//Get and Set
+	public Long getNro() {
 		return nro;
 	}
-
-	public void setNro(long nro) {
+	public void setNro(Long nro) {
 		this.nro = nro;
 	}
-
 	public Date getFecha() {
 		return fecha;
 	}
-
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-
 	public int getNroFilas() {
 		return nroFilas;
 	}
-
 	public void setNroFilas(int nroFilas) {
 		this.nroFilas = nroFilas;
 	}
-
 	public int getNroAsientosPorFila() {
 		return nroAsientosPorFila;
 	}
-
 	public void setNroAsientosPorFila(int nroAsientosPorFila) {
 		this.nroAsientosPorFila = nroAsientosPorFila;
 	}
-
 	public String getTipoVuelo() {
 		return tipoVuelo;
 	}
-
 	public void setTipoVuelo(String tipoVuelo) {
 		this.tipoVuelo = tipoVuelo;
 	}
-
 	public String getDestino() {
 		return destino;
 	}
-
 	public void setDestino(String destino) {
 		this.destino = destino;
 	}
-
-	public String getOrigen() {
-		return origen;
+	
+	public Vuelo toPojo() 
+	{
+		Vuelo v = new Vuelo();
+		v.setNro(this.getNro());
+		v.setFecha(this.getFecha());
+		v.setDestino(this.getDestino());
+		v.setNroFilas(this.getNroFilas());
+		v.setNroAsientosPorFila(this.getNroAsientosPorFila());
+		v.setTipoVuelo(this.getTipoVuelo());
+		return v;
 	}
-
-	public void setOrigen(String origen) {
-		this.origen = origen;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
+	
 
 }
