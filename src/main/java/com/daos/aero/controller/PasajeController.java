880516f3f1faa5ca1749a6c82a425495ffa746ca
Cristian Flury <cristianflury@gmail.com>
2023-06-21 15:05:32 -0300

    Elimino import - Servico Pasaje Listo

--- src/main/java/com/daos/aero/controller/PasajeController.java
+++ src/main/java/com/daos/aero/controller/PasajeController.java
@@ -1,11 +1,9 @@
 package com.daos.aero.controller;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
--- src/main/java/com/daos/aero/repository/PasajeRepository.java
+++ src/main/java/com/daos/aero/repository/PasajeRepository.java
@@ -3,7 +3,6 @@ package com.daos.aero.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.daos.aero.model.Pasaje;
 import com.daos.aero.model.Vuelo;
 
